91f80053560ed1dfa9634505e6cdc16786430228
Andre Weissflog <floooh@gmail.com>
2020-12-15 18:58:55 +0100

    initial webpage test

--- sokol/sokol.m
+++
@@ -1,8 +0,0 @@
-// sokol implementation library on macOS (must be compiled as ObjC)
-#define SOKOL_IMPL
-#define SOKOL_METAL
-#include "sokol_app.h"
-#include "sokol_gfx.h"
-#include "sokol_audio.h"
-#include "sokol_time.h"
-#include "sokol_glue.h"
